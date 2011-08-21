bf37a5dbca788ff845c8cece6d8a82d6f2b65ad7
Chris Salch <emeraldd.chris@gmail.com>
2011-08-21 10:27:58 -0500

    Added org-mode configuration

--- startup.el
+++ startup.el
@@ -154,3 +154,9 @@
 (color-theme-initialize)
 (eval-after-load "color-theme" '(when window-system (color-theme-parus)))
 
+;; Setup org-mode
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)

13cd3ba9673e218f1e094a1388d44d66df91e7f0
Chris Salch <emeraldd.chris@gmail.com>
2011-09-17 12:46:36 -0500

    Turn off org-mode for now.

--- startup.el
+++ startup.el
@@ -155,8 +155,8 @@
 (eval-after-load "color-theme" '(when window-system (color-theme-parus)))
 
 ;; Setup org-mode
-(require 'org-install)
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
+;; (require 'org-install)
+;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+;; (define-key global-map "\C-cl" 'org-store-link)
+;; (define-key global-map "\C-ca" 'org-agenda)
+;; (setq org-log-done t)
